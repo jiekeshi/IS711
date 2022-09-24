fa2b850cad613bae1fa9d9854e407cf13a145d5f
Jieke SHI <jiekeshi125@gmail.com>
2022-09-25 02:47:45 +0800

    update

--- Value_iteration.py
+++ Value_iteration.py
@@ -10,65 +10,30 @@ print_name_alias = True
 
 # MDP config
 grid_rows, grid_cols = 2,2
-convergence_tol = 0.01
+convergence_tol = 10
 discount_factor = 0.95
 
 
 ### MDP classes
 
 class State:
-    def __init__(self, pos, is_absorb_state, reward, name_alias=None):
-        assert len(name_alias) <= name_alias_max if name_alias is not None else True  # Allow single char name only
+    def __init__(self, pos):
         self.pos = pos
-        self.is_absorb_state = is_absorb_state
-        self.reward = reward
-        self.name_alias = name_alias
         self.value = 0
         self.value_old = 0
         self.is_converged = True
         self.policy = "--"
 
 class Transition:
-    def __init__(self, direction, pos_delta, transition_prob):
-        self.direction = direction
-        self.pos_delta = pos_delta
+    def __init__(self, start, end, transition_prob, reward):
+        self.start = start
+        self.end = end
         self.transition_prob = transition_prob
+        self.reward = reward
 
 class Action:
-    def __init__(self, direction, transitions):
-        self.direction = direction
+    def __init__(self, transitions):
         self.transitions = transitions
-
-
-### Print functions
-
-def print_states(states, value_to_print, print_name_alias):
-    if value_to_print == "value":
-        f_print_value = lambda state: state.value
-    elif value_to_print == "policy":
-        f_print_value = lambda state: state.policy
-    else:
-        raise ValueError("value_to_print must be one of {0}".format(["value", "policy"]))
-
-    for i in reversed(range(1, grid_rows+1)):   # Print like a "mathematical graph"
-        out_str = ""
-        for j in range(1, grid_cols+1):
-            pos = (i, j)
-            if pos in states:
-                state = states[pos]
-                s = str(f_print_value(state))[:name_alias_max]
-                if print_name_alias and state.is_absorb_state:
-                    s = str(state.name_alias)
-            else:
-                s = ""
-            out_str += s.ljust(grid_padding)
-        print(out_str)
-
-def print_states_value(states):
-    print_states(states, "value", print_name_alias)
-
-def print_states_policy(states):
-    print_states(states, "policy", print_name_alias)
 
 
 ### Main
@@ -78,52 +43,65 @@ def main():
 
     ### Create states
     states = {}
-    for i in range(1, grid_rows+1):
-        for j in range(1, grid_cols+1):
-            pos = (i,j)
-            states[pos] = State(pos, is_absorb_state=False, reward=-0.025)
-    # states[(3,4)] = State((3,4), is_absorb_state=True, reward=1, name_alias="Goal")   # End goal
-    # states[(2,4)] = State((2,4), is_absorb_state=True, reward=-1, name_alias="Tiger")   # Tiger
-    # states.pop((2,2))   # Wall
+    places = ["L1", "L2", "L3", "L4"]
+    for i in places:
+        states[i] = State(i)
 
     ### Create actions
-    pos_deltas = {
-        "N": (1,0), "S": (-1,0),
-        "E": (0,1), "W": (0,-1),
-    }
     actions = {}
-    actions["N"] = Action("N", transitions=[
-        Transition("N", pos_deltas["N"], 0.8), Transition("W", pos_deltas["W"], 0.1), Transition("E", pos_deltas["E"], 0.1),
+    actions["Pickup"] = Action(transitions=[
+        Transition("L1", "L1", 0.7, 0),
+        Transition("L1", "L2", 0.15, 19),
+        Transition("L1", "L3", 0.09, 8.5),
+        Transition("L1", "L4", 0.96, 7.75),
+        Transition("L2", "L2", 0.5, 0),
+        Transition("L2", "L1", 0.2, 9),
+        Transition("L2", "L3", 0.3, 6.25),
+        Transition("L3", "L3", 0.4, 0),
+        Transition("L3", "L1", 0.18, 13.5),
+        Transition("L3", "L4", 0.42, 7.8),
+        Transition("L4", "L4", 0.2, 0),
+        Transition("L4", "L1", 0.32, 9.75),
+        Transition("L4", "L2", 0.48, 4),
     ])
-    actions["S"] = Action("S", transitions=[
-        Transition("S", pos_deltas["S"], 0.8), Transition("W", pos_deltas["W"], 0.1), Transition("E", pos_deltas["E"], 0.1),
+    actions["MoveToL1"] = Action(transitions=[
+        Transition("L2", "L1", 1, -1),
+        Transition("L3", "L1", 1, -1.5),
+        Transition("L4", "L1", 1, -1.25)
     ])
-    actions["E"] = Action("E", transitions=[
-        Transition("E", pos_deltas["E"], 0.8), Transition("N", pos_deltas["N"], 0.1), Transition("S", pos_deltas["S"], 0.1),
+    actions["MoveToL2"] = Action(transitions=[
+        Transition("L1", "L2", 1, -1),
+        Transition("L4", "L2", 1, -1)
     ])
-    actions["W"] = Action("W", transitions=[
-        Transition("W", pos_deltas["W"], 0.8), Transition("N", pos_deltas["N"], 0.1), Transition("S", pos_deltas["S"], 0.1),
+    actions["MoveToL3"] = Action(transitions=[
+        Transition("L1", "L3", 1, -1.5),
+        Transition("L2", "L3", 1, -1.75)
+    ])
+    actions["MoveToL4"] = Action(transitions=[
+        Transition("L1", "L4", 1, -1.25),
+        Transition("L3", "L4", 1, -1.2)
     ])
 
     ### Value Iteration
     # Print initial values
-    print_states_value(states)
+    print(states["L1"].value)
+    print(states["L2"].value)
+    print(states["L3"].value)
+    print(states["L4"].value)
     print("Initial state values" + "\n")
 
     i = 0
-    while True:
+    for round in range(2):
         # Value iteration (estimate state values)
         for state in states.values():
-            if state.is_absorb_state: continue
             expected_utils = []
-            for action in actions.values():
+            for action_name, action in actions.items():
                 expected_util = 0
                 for t in action.transitions:
-                    pos_new = tuple(a + b for a, b in zip(state.pos, t.pos_delta))
-                    pos_new = pos_new if pos_new in states else state.pos   # new pos if within grid and not wall
-                    state_new = states[pos_new]
-                    expected_util += t.transition_prob * (state_new.reward + (state_new.value_old * discount_factor))
-                expected_utils.append((expected_util, action.direction,))
+                    if t.start == state.pos:
+                        state_new = states[t.end]
+                        expected_util += t.transition_prob * (t.reward + (state_new.value_old * discount_factor))
+                expected_utils.append((expected_util, action_name))
             best_action = max(expected_utils, key=lambda x: x[0])   # Action with max value / expected utility
             state.value, state.policy = best_action
 
@@ -133,41 +111,18 @@ def main():
             state.value_old = state.value
         # Print
         i += 1
-        print_states_value(states)
+        print(states["L1"].value)
+        print(states["L2"].value)
+        print(states["L3"].value)
+        print(states["L4"].value)
         print("Iteration: {0}".format(i) + "\n")
         # Convergence check
         is_converged = all([state.is_converged for state in states.values()])
         if is_converged: break
 
-    print_states_policy(states)
     print("Best policy" + "\n")
     print("Time taken is {:.4f} seconds".format(time() - t0))
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
